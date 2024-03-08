c27cb17b54461b24b40ad4dd7ddcb16d5933138a
阿男 <l.weinan@gmail.com>
2024-03-09 00:54:38 +0800

    Add wildfly start/stop

--- setup.sh
+++ setup.sh
@@ -39,6 +39,40 @@ cp $JBERET_PORTING_DIR/src/main/resources/runners/se-classpath/pom.xml $BATCH_TC
 cp $JBERET_PORTING_DIR/src/main/resources/runners/platform-arquillian/pom.xml $BATCH_TCK_DIR/runners/platform-arquillian/pom.xml
 cp $JBERET_PORTING_DIR/src/main/resources/runners/platform-arquillian/src/test/resources/arquillian.xml $BATCH_TCK_DIR/runners/platform-arquillian/src/test/resources/arquillian.xml
 
+
+# Run SE tests
 pushd $BATCH_TCK_DIR/runners/se-classpath
 mvn install -Dversion.org.jberet.jberet-core=${jberet_ver}
 popd
+
+# Run integration tests
+
+# start WildFly server
+pushd $JBOSS_HOME/bin
+./standalone.sh &
+
+sleep 10
+NUM=0
+while true
+do
+NUM=$[$NUM + 1]
+if (("$NUM" > "6")); then
+#    echo "Application server failed to start up! Will run tests anyway"
+#    netstat -an
+    echo "Application server failed to start up!"
+    exit 1
+#    echo "try to run tests anyway even though server is not running"
+#    break
+fi
+
+if ./jboss-cli.sh --connect command=':read-attribute(name=server-state)' | grep running; then
+    echo "server is running"
+    break
+fi
+    echo "server is not yet running"
+    sleep 10
+done
+popd
+
+# stop WildFly server
+$JBOSS_HOME/bin/jboss-cli.sh --connect --commands="undeploy jberet-tck-porting.jar, shutdown"
